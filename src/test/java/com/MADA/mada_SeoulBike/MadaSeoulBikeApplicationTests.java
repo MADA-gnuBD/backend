f6ec7e3af52e41c5f97dc5bbef47fcecfec92c9b
saehe park <119057831+popcifox@users.noreply.github.com>
2025-07-16 09:54:06 +0900

    Initial commit

---
+++ src/test/java/com/MADA/mada_SeoulBike/MadaSeoulBikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MADA.mada_SeoulBike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadaSeoulBikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
